a0829e7fd578998f392652005afca48f50d9fcda
Takuya Sudo <t-sudo@goem.co.jp>
2021-10-16 22:41:20 +0900

    swift package init

---
+++ Sources/swifttest/swifttest.swift
@@ -0,0 +1,6 @@
+public struct swifttest {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/swifttestTests/swifttestTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swifttest
+
+final class swifttestTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swifttest().text, "Hello, World!")
+    }
+}
